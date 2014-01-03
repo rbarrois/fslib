3ce958f0e7d0e1ebdfc81c89fad46c44b56cbd39
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-01-03 22:51:57 +0100

    Version bump to 0.2.1

--- fslib/__init__.py
+++ fslib/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2011-2013 Raphaël Barrois
 # This code is distributed under the two-clause BSD License.
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Raphaël Barrois <raphael.barrois+fslib@polytechnique.org>'
 
 
