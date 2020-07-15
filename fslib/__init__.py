b7d0bb3431fe23530ebd5043d0d518b5b2d108be
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-07-15 23:01:02 +0200

    Release fslib v0.3.4

--- fslib/__init__.py
+++ fslib/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2011-2020 Raphaël Barrois
 # This code is distributed under the two-clause BSD License.
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __author__ = 'Raphaël Barrois <raphael.barrois+fslib@polytechnique.org>'
 
 
